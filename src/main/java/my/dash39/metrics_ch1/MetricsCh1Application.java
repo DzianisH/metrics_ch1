e8acb4fad95fa3c982068d5ab2387c21f0c84496
Dzianis Haurylavets <dzianis.haurylavets@ab-soft.net>
2021-11-27 19:11:20 +0200

    Init project

---
+++ src/main/java/my/dash39/metrics_ch1/MetricsCh1Application.java
@@ -0,0 +1,13 @@
+package my.dash39.metrics_ch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsCh1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsCh1Application.class, args);
+    }
+
+}
---
+++ src/test/java/my/dash39/metrics_ch1/MetricsCh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.dash39.metrics_ch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsCh1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
